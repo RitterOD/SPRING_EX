63593b9a0d288137c1c2038f9d0e5579362707d9
Alexander Maslov <ritterod@gmail.com>
2021-12-03 12:35:17 +0300

    SPRING_EX.security-hello: add spring-security example
    with user and random password
    
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

---
+++ security-hello/src/main/java/org/maslov/controller/HelloController.java
@@ -0,0 +1,13 @@
+package org.maslov.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    String greeting(){
+        return "hello from spring security!";
+    }
+}
