90d116a72510f835a9dbb039bca4c449f421c70a
Alexander Maslov <ritterod@gmail.com>
2021-12-13 12:54:13 +0300

    security-form-login: borrow code from security-db project
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

---
+++ security-form-login/src/main/java/org/maslov/SecurityFormLoginApp.java
@@ -0,0 +1,11 @@
+package org.maslov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityDbApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityDbApp.class);
+    }
+}
---
+++ security-form-login/src/main/java/org/maslov/configuration/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package org.maslov.configuration;
+
+import org.maslov.controller.CustomEntryPoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+/*
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsService = new InMemoryUserDetailsManager();
+        var user = User.withUsername("John").password("12345").authorities("read").build();
+        userDetailsService.createUser(user);
+        return userDetailsService;
+    }
+*/
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic(c -> {
+            c.realmName("OTHER");
+            c.authenticationEntryPoint(new CustomEntryPoint());
+        });
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+}
