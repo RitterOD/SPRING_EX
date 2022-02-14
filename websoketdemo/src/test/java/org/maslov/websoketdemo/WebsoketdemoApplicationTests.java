b3166b0451683a1ede43ec239a0005818ea9aa76
Alexander Maslov <ritterod@gmail.com>
2022-02-14 20:27:28 +0300

    websocket-demo: first implementation
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

---
+++ websoketdemo/src/test/java/org/maslov/websoketdemo/WebsoketdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maslov.websoketdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsoketdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
