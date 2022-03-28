37c1fa4c5847ee099b8854be635bec346044df65
Alexander Maslov <ritterod@gmail.com>
2022-03-28 21:43:06 +0300

    check-kafka: first implementation
    Embedded integration test worked
    
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

---
+++ check-kafka/src/main/java/org/maslov/check/kafka/CheckKafkaApp.java
@@ -0,0 +1,13 @@
+package org.maslov.check.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckKafkaApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckKafkaApp.class, args);
+	}
+
+}
