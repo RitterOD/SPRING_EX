409b74f0261499ed5510953fc824c486f5c25800
Alexander Maslov <ritterod@gmail.com>
2022-04-26 12:33:34 +0300

    simpleKafkaConsumer: Read messages
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

---
+++ kafka/simpeconsumer/src/main/java/org/maslov/SimpleKafkaConsumer.java
@@ -0,0 +1,59 @@
+package org.maslov;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class SimpleKafkaConsumer {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        String server = "localhost:9092";
+        String topicName = "test.topic";
+        String groupName = "test.group";
+
+        final Properties props = new Properties();
+
+        props.put(ConsumerConfig.GROUP_ID_CONFIG,
+                groupName);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                server);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                LongDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getName());
+
+        final Consumer<Long, String> consumer =
+                new KafkaConsumer<>(props);
+
+        TopicPartition tp = new TopicPartition(topicName, 0);
+        List<TopicPartition> tps = Arrays.asList(tp);
+        consumer.assign(tps);
+        consumer.seekToBeginning(tps);
+
+        int i = 0;
+        while (i++ < 100) {
+            ConsumerRecords<Long, String> consumerRecords = consumer.poll(30000);
+            if (!consumerRecords.isEmpty()) {
+                System.out.println("SUCCESS");
+                for(ConsumerRecord record: consumerRecords) {
+                    System.out.println(record.value());
+                }
+
+            }
+            consumer.commitAsync();
+        }
+        consumer.close();
+    }
+}
