d46c4679b50a3c5d8e48dc28c50edb73a541c304
Alexander Maslov <ritterod@gmail.com>
2022-05-18 23:23:54 +0300

    antlr4-demo: Simple test passed
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

--- antrl4-demo/src/main/java/org/maslov/antrl4demo/calcparser/CalcParserListener.java
+++ antrl4-demo/src/main/java/org/maslov/antrl4demo/calcparser/CalcParserListener.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 public class CalcParserListener extends CalcBaseListener {
 
     private int value;
+    private int action = 1;
 
     public int getValue() {
         return value;
@@ -13,13 +14,24 @@ public class CalcParserListener extends CalcBaseListener {
 
     @Override
     public void enterList(CalcParser.ListContext ctx) {
-      log.info( ctx.DIGIT().getSymbol().getText());
+      log.info( "enterList" + ctx.DIGIT().getSymbol().getText());
+      int digit = Integer.parseInt(ctx.DIGIT().getSymbol().getText());
+      value += digit * action;
+      if (ctx.getChild(1) != null) {
+          if (ctx.getChild(1).getText().equals("+")) {
+              action = 1;
+          } else if (ctx.getChild(1).getText().equals("-")) {
+              action = -1;
+          } else {
+              throw new IllegalStateException("Illegal token. Token text:" + ctx.getChild(1).getText());
+          }
+      }
 
     }
 
     @Override
     public void exitList(CalcParser.ListContext ctx) {
-        log.info( ctx.DIGIT().getSymbol().getText());
+        log.info( "exitList" + ctx.DIGIT().getSymbol().getText());
     }
 
 
