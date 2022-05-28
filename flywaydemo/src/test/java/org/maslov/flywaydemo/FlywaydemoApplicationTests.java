5d917023de083486be316bbc6434d03ae76d0ec0
Alexander Maslov <ritterod@gmail.com>
2022-05-28 12:37:06 +0300

    FlywayDemo: Initial commit
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

---
+++ flywaydemo/src/test/java/org/maslov/flywaydemo/FlywaydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maslov.flywaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywaydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
