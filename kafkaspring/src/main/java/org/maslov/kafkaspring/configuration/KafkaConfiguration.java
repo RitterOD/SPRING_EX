d0e321a5f747f50fe72a09cdc35971a65dc4ec6d
Alexander Maslov <ritterod@gmail.com>
2022-07-05 13:15:38 +0300

    KafkaSping: example of integration between spring and kafka
    Add consumer
    
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

--- kafkaspring/src/main/java/org/maslov/kafkaspring/configuration/KafkaConfiguration.java
+++ kafkaspring/src/main/java/org/maslov/kafkaspring/configuration/KafkaConfiguration.java
@@ -1,20 +1,24 @@
 package org.maslov.kafkaspring.configuration;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.maslov.kafkaspring.model.Message;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+//@EnableKafka
 public class KafkaConfiguration {
 
     @Bean
@@ -35,4 +39,28 @@ public class KafkaConfiguration {
     public KafkaTemplate<String, Message> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
     }
+
+    @Bean
+    public ConsumerFactory<String, Message> consumerFactory() {
+        String server = "localhost:9092";
+        String topicName = "test.topic";
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                server);
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "GROUP-0");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<>(config, new StringDeserializer(), new JsonDeserializer<>(Message.class));
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory kafkaListenerContainerFactory(ConsumerFactory consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory cKLCF = new ConcurrentKafkaListenerContainerFactory();
+        cKLCF.setConsumerFactory(consumerFactory);
+        return cKLCF;
+    }
+
+
 }
--- kafkaspring/src/main/java/org/maslov/kafkaspring/controller/KafkaSimpleController.java
+++ kafkaspring/src/main/java/org/maslov/kafkaspring/controller/KafkaSimpleController.java
@@ -1,22 +1,41 @@
 package org.maslov.kafkaspring.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.maslov.kafkaspring.model.Message;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class KafkaSimpleController {
 
     private final KafkaTemplate kafkaTemplate;
 
-    public KafkaSimpleController(KafkaTemplate kafkaTemplate) {
+    private final ObjectMapper objectMapper;
+
+    public KafkaSimpleController(KafkaTemplate kafkaTemplate, ObjectMapper objectMapper) {
         this.kafkaTemplate = kafkaTemplate;
+        this.objectMapper = objectMapper;
     }
 
     @PostMapping("/api/message")
     public void post(@RequestBody Message message) {
         kafkaTemplate.send("springTopic", message);
+    }
+
+    @KafkaListener(topics={"springTopic"})
+    public void listen(Message message) {
+        try {
+            String tmp = objectMapper.writeValueAsString(message);
+            log.info("Receive message: " + tmp);
+        } catch (JsonProcessingException e) {
+            log.info(e.getMessage());
+        }
+
     }
 }
