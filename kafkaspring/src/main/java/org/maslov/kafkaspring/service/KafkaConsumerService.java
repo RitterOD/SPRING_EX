3be1bc9ac8e420fcd1f2f26a5309792e0b550e59
Alexander Maslov <ritterod@gmail.com>
2022-07-06 13:36:19 +0300

    KafkaSpring: add reading of offset header
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

--- kafkaspring/src/main/java/org/maslov/kafkaspring/service/KafkaConsumerService.java
+++ kafkaspring/src/main/java/org/maslov/kafkaspring/service/KafkaConsumerService.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.maslov.kafkaspring.configuration.AppConstant;
 import org.maslov.kafkaspring.model.Message;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Service;
 
@@ -26,9 +27,10 @@ public class KafkaConsumerService {
 
     @KafkaListener(topics={AppConstant.TOPIC_NAME})
     public void listen(Message message,
-                       @Header(CUSTOM_TIMESTAMP_HEADER) String ts) {
+                       @Header(CUSTOM_TIMESTAMP_HEADER) String ts, @Header(KafkaHeaders.OFFSET) String offsetHeader) {
         try {
             String tmp = objectMapper.writeValueAsString(message);
+            log.info("offset header:" + offsetHeader);
             log.info("Receive message: " + tmp);
             log.info(CUSTOM_TIMESTAMP_HEADER + ": " + ts);
         } catch (JsonProcessingException e) {
