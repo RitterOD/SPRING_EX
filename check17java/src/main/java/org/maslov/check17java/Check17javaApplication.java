63a69eee42a499195be1747b69f60f1bd495f16d
Alexander Maslov <ritterod@gmail.com>
2022-08-16 15:28:51 +0300

    check17java: 1)add SpringBootApplication based on JDK 17.
    1.1)add all files
    
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

---
+++ check17java/src/main/java/org/maslov/check17java/Check17javaApplication.java
@@ -0,0 +1,13 @@
+package org.maslov.check17java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Check17javaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Check17javaApplication.class, args);
+	}
+
+}
---
+++ check17java/src/test/java/org/maslov/check17java/Check17javaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maslov.check17java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Check17javaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
