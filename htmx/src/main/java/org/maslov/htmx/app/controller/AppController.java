bfc4c7f46162ec8a2636e0f4784d63d7ed27efd3
Alexander Maslov <ritterod@gmail.com>
2022-10-26 14:48:12 +0300

    htmx: search example works
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

--- htmx/src/main/java/org/maslov/htmx/app/controller/AppController.java
+++ htmx/src/main/java/org/maslov/htmx/app/controller/AppController.java
@@ -1,12 +1,15 @@
 package org.maslov.htmx.app.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.maslov.htmx.app.model.Message;
 import org.maslov.htmx.app.model.mapper.MessageMapper;
 import org.maslov.htmx.app.service.MessageService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -28,5 +31,13 @@ public class AppController {
         var msgs = messageService.findAll();
         var views = messageMapper.map(msgs);
         return new ModelAndView("index", Map.of("messagesView", views));
+    }
+
+    @GetMapping("/search")
+    public ModelAndView search(@RequestParam String search_pattern) {
+        log.info("IN SEARCH");
+        List<Message> msgs = messageService.findByMessageContaining(search_pattern);
+        var views = messageMapper.map(msgs);
+        return new ModelAndView("fragments/search :: search-result", Map.of("messagesView", views));
     }
 }
--- htmx/src/main/java/org/maslov/htmx/app/controller/AppRestController.java
+++ htmx/src/main/java/org/maslov/htmx/app/controller/AppRestController.java
@@ -1,14 +1,25 @@
 package org.maslov.htmx.app.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.maslov.htmx.app.model.Message;
+import org.maslov.htmx.app.service.MessageService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@Slf4j
 public class AppRestController {
 
+    private final MessageService messageService;
+
+    public AppRestController(MessageService messageService) {
+        this.messageService = messageService;
+    }
 
     @PostMapping("/clicked")
     public String clicked() {
         return "<h1> Random number from server: " + Math.random() + "</h1>";
     }
+
 }
