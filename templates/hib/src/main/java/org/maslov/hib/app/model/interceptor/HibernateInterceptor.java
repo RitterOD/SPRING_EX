55fa1dc37be32bbab09455b0a7107a4fe383128f
Alexander Maslov <ritterod@gmail.com>
2022-11-19 14:02:05 +0300

    hibernate-templates: add HibernateInterceptor: NOT WORKED
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

---
+++ templates/hib/src/main/java/org/maslov/hib/app/model/interceptor/HibernateInterceptor.java
@@ -0,0 +1,31 @@
+package org.maslov.hib.app.model.interceptor;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.CallbackException;
+import org.hibernate.Interceptor;
+import org.hibernate.type.Type;
+import org.maslov.hib.app.model.Message;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Slf4j
+@Component
+public class HibernateInterceptor implements Interceptor, Serializable {
+
+    @Override
+    public boolean onLoad(Object entity, Object id, Object[] state, String[] propertyNames, Type[] types)
+            throws CallbackException {
+        log.info("In hibernate interceptor");
+        if (entity instanceof Message) {
+            var message = (Message) entity;
+            log.info("message.id: " + message.getId());
+            log.info("message.test: " + message.getText());
+        } else {
+            log.error("Unsupported type of entity: " + entity.getClass().getSimpleName());
+        }
+        return false;
+    }
+
+}
