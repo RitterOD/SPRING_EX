aa69222e0acf873540d701e4c396c0f278bf5dcd
Alexander Maslov <ritterod@gmail.com>
2022-12-29 13:54:17 +0300

    sec-template: Initial commit
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

---
+++ templates/sec/src/main/java/org/maslov/template/model/DiagramEntry.java
@@ -0,0 +1,29 @@
+package org.maslov.template.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DiagramEntry {
+    @Id
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private Long text;
+
+    @OneToMany(mappedBy = "father")
+    private List<DiagramEntry> entries;
+
+    @ManyToOne
+    @JoinColumn(name = "father_id")
+    private DiagramEntry father;
+}
