5e4beac5fcd5807353941c4c741d622cb138807f
Alexander Maslov <ritterod@gmail.com>
2023-01-09 12:27:45 +0300

    SPRIN_EX: cleanup: remove web application. old unused
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

--- web/src/main/java/org/maslov/web/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package org.maslov.web.dao;
-import org.maslov.web.entity.User;
-
-public interface UserDao {
-    User findByLogin(String login);
-    Boolean save(User user);
-}
--- web/src/main/java/org/maslov/web/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package org.maslov.web.service;
-
-import org.maslov.web.entity.User;
-
-public interface UserService {
-    public String login(User user);
-    public String registration(User user);
-}
