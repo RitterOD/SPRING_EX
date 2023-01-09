05b14c549c76a9d7d9715094af58af923e6313fa
Alexander Maslov <ritterod@gmail.com>
2023-01-09 13:40:25 +0300

    SPRING_EX: reorganize repository: fix issues with unversioned file
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

---
+++ mongo-example/src/test/java/org/maslov/mongo/MongoExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.maslov.mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
