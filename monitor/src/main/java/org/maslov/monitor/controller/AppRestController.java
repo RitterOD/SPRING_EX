be05bfef536c23a7c261306f8a6c06f289587ff1
Alexander Maslov <ritterod@gmail.com>
2023-01-30 09:51:38 +0300

    monitor: 1) add custom metric to controller
    2) fix issues with grafana
    
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

--- monitor/src/main/java/org/maslov/monitor/controller/AppRestController.java
+++ monitor/src/main/java/org/maslov/monitor/controller/AppRestController.java
@@ -1,5 +1,9 @@
 package org.maslov.monitor.controller;
 
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AppRestController {
 
+  private AtomicInteger visitCounter;
+
+  public Supplier<Number> getVisitCounter() {
+    return ()-> visitCounter.get();
+  }
+
+  public AppRestController(MeterRegistry registry) {
+    this.visitCounter = new AtomicInteger(0);
+    Gauge.builder("user.visits", getVisitCounter())
+      .tag("version", "v1")
+      .description("user visits on main page")
+      .register(registry);
+  }
+
   @GetMapping("/")
   public ResponseEntity<String> index() {
+    visitCounter.incrementAndGet();
+
     return ResponseEntity.ok().body("Monitor Application");
   }
 }
