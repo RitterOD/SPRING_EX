86d7b287e417ca0df654388f3da15508da62e9d9
Alexander Maslov <ritterod@gmail.com>
2023-01-31 12:24:56 +0300

    openapi-generator: 1) add replacer plugin.
    2) add TestController
    
    Signed-off-by: Maslov Alexander <ritterod@gmail.com>

---
+++ openapi/openapi/src/main/java/org/maslov/openapi/api/TestController.java
@@ -0,0 +1,9 @@
+package org.maslov.openapi.api;
+
+import org.maslov.openapi.api.controller.HelloOpenApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController implements HelloOpenApi {
+
+}
